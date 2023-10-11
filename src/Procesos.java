1169d7eeaf5a29adcdb5631f6861457d6e6f48a3
andrwe455 <andrwe455@gmail.com>
2023-10-11 09:45:38 -0500

    clase procesos

---
+++ src/Procesos.java
@@ -0,0 +1,19 @@
+public class Procesos {
+
+    private int R;
+    private int G;
+    private int B;
+    private int A;
+
+    public Procesos() {
+        this.R = 10;
+        this.G = 20;
+        this.B = 15;
+        this.A = 35;
+    }
+
+    public void P1()
+    {
+
+    }
+}
