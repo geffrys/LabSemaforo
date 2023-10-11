7ae3d3439e38b2c371e72cd09cfa8b5d4f9e94ec
andrwe455 <andrwe455@gmail.com>
2023-10-11 10:30:26 -0500

    Co-authored-by: andrwe455 <andrwe455@gmail.com>

--- src/Procesos.java
+++ src/Procesos.java
@@ -1,18 +1,50 @@
-public class Procesos {
+public class Procesos extends Semaforos{
 
     private int R;
     private int G;
-    private int B;
-    private int A;
+    private int T;
+    private int H;
 
     public Procesos() {
         this.R = 10;
         this.G = 20;
-        this.B = 15;
-        this.A = 35;
+        this.T = 15;
+        this.H = 35;
     }
 
     public void P1()
+    {
+
+    }
+    public void P11()
+    {
+
+    }
+    public void P12()
+    {
+
+    }
+    public void P13()
+    {
+
+    }
+    public void P21()
+    {
+
+    }
+    public void P22()
+    {
+
+    }
+    public void P3()
+    {
+
+    }
+    public void P31()
+    {
+
+    }
+    public void P32()
     {
 
     }
