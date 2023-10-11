95e589998ccaf9d845d96d9769a8ad92e17f29ac
andrwe455 <andrwe455@gmail.com>
2023-10-11 09:39:36 -0500

    Clase semaforos

--- src/Semaforos.java
+++ src/Semaforos.java
@@ -6,4 +6,11 @@ public class Semaforos {
     private Semaphore B;
     private Semaphore C;
     private Semaphore D;
+
+    public Semaforos() {
+        this.A = new Semaphore(1);
+        this.B = new Semaphore(0);
+        this.C = new Semaphore(0);
+        this.D = new Semaphore(0);
+    }
 }
